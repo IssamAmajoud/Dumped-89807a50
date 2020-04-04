2efadc8d49fb5521280b12b3b6bcdd5741f7cfde
IssamAmajoud <45203821+IssamAmajoud@users.noreply.github.com>
2020-04-04 20:55:45 +0200

    Add files via upload

---
+++ dumped.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$tekst = "Ik ben een string";
+echo var_dump($tekst) . "<br>"; 
+
+$getal = 1337;
+echo var_dump($getal) . "<br>";
+
+?>
+
+</body>
+</html>
